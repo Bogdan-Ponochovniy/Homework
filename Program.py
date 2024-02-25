80bb3cebbccaab099a2f38b556ba4ebaa76edeab
Bogdan-Ponochovniy <160404102+Bogdan-Ponochovniy@users.noreply.github.com>
2024-02-25 11:13:41 +0200

    Add files via upload

---
+++ Program.py
@@ -0,0 +1,13 @@
+numbers = [7, 3, 5, 1, 9, 2, 6, 4, 8]
+
+numbers.sort()
+print("Відсортований список:", numbers)
+
+sum_numbers = sum(numbers)
+print("Сума значень:", sum_numbers)
+
+count_numbers = len(numbers)
+print("Кількість значень:", count_numbers)
+
+average_number = sum_numbers / count_numbers
+print("Середнє значення:", int(average_number))
